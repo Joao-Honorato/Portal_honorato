6845ed2ccc8a548b9cb66e5d59d07f9b9296ec0a
Joao-Honorato <167466646+Joao-Honorato@users.noreply.github.com>
2024-06-01 12:20:53 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,1 +1,21 @@
+<?php
 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá</title>
+</head>
+<body>
+    <form action="form2.php" method = "POST">
+    <div id = "nomeanuncio" name = "blocoanuncio">
+    Nome do anúncio <input type=text name= "nomeAnuncio" id="nomeAnuncio"><br>
+        <input type="submit" name = "submit">
+    </div>
+    </form>
+    
+</body>
+</html>
